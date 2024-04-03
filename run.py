fa6afed13ced333873292e7c883c48dd8f4ca8f0
Leon Potgieter <hexvallei@gmail.com>
2024-04-03 20:01:34 +0000

    update comments

--- run.py
+++ run.py
@@ -19,124 +19,123 @@ from pprint import pprint # Temp, for debugging
 
 os.system("clear")
 
-# SCOPE = [
-#     "https://www.googleapis.com/auth/spreadsheets",
-#     "https://www.googleapis.com/auth/drive.file",
-#     "https://www.googleapis.com/auth/drive"
-#     ]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-# CREDS = Credentials.from_service_account_file('creds.json')
-# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-# SHEET = GSPREAD_CLIENT.open('vault_guard_db')
-
-
-
-# pyhibp.set_user_agent(ua="Awesome application/0.0.1 (An awesome description)")
-# my_pass = '&5LZXuF8Yi#Q3t'
-# resp = pw.is_password_breached(password=my_pass)
-# if resp:
-#     print("Password breached!")
-#     print(f"The password {my_pass} was used {resp} time(s) before.")
-# else:
-#     print(f"The password {my_pass} is clean!.")
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('vault_guard_db')
 
 
-# # Load specific worksheet
-# x = SHEET.worksheet('test')
 
-# # Get All data 
-# pprint(x.get_all_values())
-
-# # Print Data with PrettyTables
-# table = PrettyTable()
-# table.field_names = ["ID", "Service", "Username", "Password"]
-# for i in x.get_all_values()[1:]:
-#     table.add_row(i)
-# print(table)
+pyhibp.set_user_agent(ua="Awesome application/0.0.1 (An awesome description)")
+my_pass = '&5LZXuF8Yi#Q3t'
+resp = pw.is_password_breached(password=my_pass)
+if resp:
+    print("Password breached!")
+    print(f"The password {my_pass} was used {resp} time(s) before.")
+else:
+    print(f"The password {my_pass} is clean!.")
 
 
-# # Menu Generation
-# title = Back.GREEN + " * Title * " + Style.RESET_ALL
-# subtitle = Fore.RED + Back.YELLOW + " * Subtitle * " + Style.RESET_ALL
-# menu = ConsoleMenu(title, subtitle)
-# f_item = FunctionItem("Option1", None)
-# s_item = FunctionItem("Option2", None)
-# menu.append_item(f_item)
-# menu.append_item(s_item)
-# menu.show()
+# Load specific worksheet
+x = SHEET.worksheet('test')
+
+# Get All data 
+pprint(x.get_all_values())
+
+# Print Data with PrettyTables
+table = PrettyTable()
+table.field_names = ["ID", "Service", "Username", "Password"]
+for i in x.get_all_values()[1:]:
+    table.add_row(i)
+print(table)
 
 
-# # My own password star display function
-# passw = ''
-# print("Please Enter your password...")
-# while True:
-#     key = getch.getch()
-#     if key == '\n':
-#         break
-#     sys.stdout.write('*')
-#     sys.stdout.flush()
-#     passw += str(key)
-# print('\n')
-# print(passw)
+# Menu Generation
+title = Back.GREEN + " * Title * " + Style.RESET_ALL
+subtitle = Fore.RED + Back.YELLOW + " * Subtitle * " + Style.RESET_ALL
+menu = ConsoleMenu(title, subtitle)
+f_item = FunctionItem("Option1", None)
+s_item = FunctionItem("Option2", None)
+menu.append_item(f_item)
+menu.append_item(s_item)
+menu.show()
 
 
-# # Get specific row
-# print('\n')
-# y = x.get_all_values()
-# for i in y:
-#     if i[1] == 'yahoo':
-#         print(i)
+# My own password star display function
+passw = ''
+print("Please Enter your password...")
+while True:
+    key = getch.getch()
+    if key == '\n':
+        break
+    sys.stdout.write('*')
+    sys.stdout.flush()
+    passw += str(key)
+print('\n')
+print(passw)
 
-# # Get specific column
-# print('\n')
-# pprint(x.col_values(4)[1:])
-# print('\n')
 
-# # Add new row
-# x.append_row(['test1', 'test2', 'test3', 'test4'])
-# pprint(x.get_all_values())
-# print('\n')
+# Get specific row
+print('\n')
+y = x.get_all_values()
+for i in y:
+    if i[1] == 'yahoo':
+        print(i)
 
-# for i in range(10):
-#     sys.stdout.write('.')
-#     sys.stdout.flush()
-#     time.sleep(0.2)
-# os.system("clear")
-# print('CHECK ADDED ROW NOW...')
+# Get specific column
+print('\n')
+pprint(x.col_values(4)[1:])
+print('\n')
 
-# # Add waiting time
-# print('\n')
-# for i in range(25):
-#     sys.stdout.write('.')
-#     sys.stdout.flush()
-#     time.sleep(0.2)
+# Add new row
+x.append_row(['test1', 'test2', 'test3', 'test4'])
+pprint(x.get_all_values())
+print('\n')
 
-# # Delete specific row
-# print('\n')
-# print('Deleting last row...')
-# x.delete_rows(len(x.get_all_values()))
-# pprint(x.get_all_values())
+for i in range(10):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    time.sleep(0.2)
+os.system("clear")
+print('CHECK ADDED ROW NOW...')
 
-# # Create New Spreadsheet
-# new_username = 'new-test-user'
-# # SHEET.add_worksheet(title=new_username, rows=1000, cols=4)
+# Add waiting time
+print('\n')
+for i in range(25):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    time.sleep(0.2)
 
-# # Raise Exception
-# try:
-#     exist = SHEET.worksheet('test')
-# except:
-#     print('User Database could not be found')
+# Delete specific row
+print('\n')
+print('Deleting last row...')
+x.delete_rows(len(x.get_all_values()))
+pprint(x.get_all_values())
 
-# # Colour Options
-# print(Fore.RED + 'some red text')
-# print(Back.GREEN + 'and with a green background')
-# print(Style.DIM + 'and in dim text')
-# print(Style.RESET_ALL)
-# print('back to normal now')
+# Create New Spreadsheet
+new_username = 'new-test-user'
+# SHEET.add_worksheet(title=new_username, rows=1000, cols=4)
 
-# Random Password Generation
+# Raise Exception
+try:
+    exist = SHEET.worksheet('test')
+except:
+    print('User Database could not be found')
 
+# Colour Options
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
+# Password Strength Checker
 def check_pass(p):
     if len(p) < 8:
         return False
@@ -148,6 +147,7 @@ def check_pass(p):
                 continue
     return all(strong_pass)
 
+# Random Password Generation
 password_length = 12
 
 print('Here we go...')
